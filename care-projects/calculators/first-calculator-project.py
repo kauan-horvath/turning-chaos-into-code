ada1825a69ee0379572d88780c17edb7ade19b31
Kauan Horvath <kauanhorvath1996@gmail.com>
2026-03-27 19:22:10 -0300

    feat: Associating pc-folder with github

---
+++ care-projects/calculators/first-calculator-project.py
@@ -0,0 +1,25 @@
+####################
+# TÍTULO
+####################
+
+import os
+
+CONST = None
+
+dict = {}
+
+# FUNCTIONS
+
+def aux_function():
+    pass
+
+def logic_function():
+    pass
+
+# Main Loop
+
+if __name__ == "__main__":
+    pass
+
+####################################
+
