7d4e74e5f43fefddd6511ea579fa9a8dcd4be538
Kauan Horvath <kauanhorvath1996@gmail.com>
2026-03-28 14:19:52 -0300

    Feat: Trying to creat a strong structure and failing

---
+++ README.md
@@ -0,0 +1,27 @@
+# Turning Chaos into Code 🌪️ ➡️ 💻
+
+This repository is a systematic log of my journey through software engineering, logic, and automation. It serves as a central hub for my Python projects, daily experiments, and technical evolution.
+
+## 📂 Project Structure
+
+I organized into specific directories to separate production-ready logic from daily learning:
+
+* **`/python/care-projects`**: End-to-end applications, refined tools, and portfolio-ready solutions. 
+    * *Example: Investment Calculators, RPG Engines, and Systems.*
+* **`/python/raw-sketches`**: Daily logs, fundamental logic tests, and conceptual sketches using my standard documentation template.
+* **`/python/easter-eggs`**: Creative snippets and personal coding experiments.
+
+## 🚀 The Methodology
+
+Every script in the `raw-sketches` folder follows a strict documentation standard, tracking:
+- **Milestones**: Goals for the session.
+- **Progress**: What was achieved.
+- **Failures**: Technical hurdles and how they were solved (The "Right Version" logic).
+
+## 🛠️ Tech Stack
+- **Language:** Python 3.x
+- **Environment:** VS Code / Windows (Local Dev)
+- **Version Control:** Git & GitHub
+
+---
+*“Simplicity is the soul of efficiency.”*
---
+++ care-projects/calculators/BLUEPRINT-TEMP.py
@@ -0,0 +1,69 @@
+'''
+    ####################
+    # PROJECT: MODULAR CALCULATOR
+    ####################
+
+    03/28 - Studying Software Engineering
+
+    MILESTONES:
+    - Create Pure Aritmetic []
+    - Input and Data Validation []
+    - Precedence Order? []
+    - Data persistance "M+" []
+
+    PROGRESS:
+    lambda in constants - SHORT EXPLAIN
+    shortif syntax - tell in a row
+    except Exception as var - Smart way to debug
+        print{var}
+
+    FAILURES:
+    WHAT - SHORT EXPLAIN
+        RIGHT VERSION
+    WHAT - SHORT EXPLAIN
+        RIGHT VERSION
+    
+    '''
+
+####################################
+import os
+a = 0
+b = 0
+
+OPERATIONS = {
+    "+": a + b,
+    "-": a - b,
+    "*": a * b,
+    "/": a / b if b!=0 else "Error: Div/0"
+}
+
+context = {
+    "last_result": 0,
+    "history": []
+}
+
+# FUNCTIONS
+
+def calculate(val1, val2, op):
+    try:
+        operation_function = OPERATIONS.get(op)
+        if operation_function:
+            return operation_function(val1, val2)
+        return "Invalid Operator"
+    
+    except Exception as e:
+        return f"Error: {e}"
+
+def logic_function():
+    pass
+
+# Main Loop
+
+if __name__ == "__main__":
+    print("--- Modular Calculator Initializing ---")
+    a = input("Digite um número:  ")
+    op = str(input("Digite um operador: "))
+    b = input("Digite outro número:  ")
+
+    print(calculate(a,b,op))
+
