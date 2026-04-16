13500e57c860be7980fa9512dca61b0e63efe581
Kauan Horvath <kauanhorvath1996@gmail.com>
2026-04-16 13:31:26 -0300

    fix: gemini version for aiconcierge

--- ai-concierge/server.py
+++ ai-concierge/server.py
@@ -51,7 +51,7 @@ def carregar_cerebro():
 # 3. Configurando a IA
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-2.5-flash",
     system_instruction=carregar_cerebro(),
 )
 
