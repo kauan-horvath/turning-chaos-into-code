664184db53c54589b28d1a087a470ca8afd8488f
Kauan Horvath <kauanhorvath1996@gmail.com>
2026-04-24 12:40:11 -0300

    chore: centralize python exercises into care-projects

--- care-projects/calculators/variacoes-calc.py
+++ care-projects/calculators/variacoes-calc.py
@@ -29,6 +29,7 @@ Range(1, num + 1) - Falhei algumas vezes na sintaxe dos dois argumentos de Range
 """
 
 ####################################
+import os
 
 
 def calcular_fatorial():
@@ -48,5 +49,64 @@ def calcular_fatorial():
         print("Erro: Por favor, insira um número inteiro válido.")
 
 
+def calcula_potencia():
+    try:
+        base = input("Digite o número da base: ")
+        base = int(base)
+
+        expoente = input("Digite o expoente: ")
+        expoente = int(expoente)
+
+        if base < 0:
+            print("A base precisa ser maior que 0")
+            return ValueError
+        elif expoente <= 1:
+            print("O Exponte precisa ser maior que 1")
+            return ValueError
+
+        resultado = base**expoente
+
+        print(f"{base} elevado à {expoente} é igual a: {resultado}")
+
+    except ValueError:
+        print("O número informado não é válido")
+
+
+# VARIAÇÃO PERCENTUAL
+
+
+def varia_percent():
+    try:
+        val_inicial = input("Digite o valor inicial: ")
+        val_inicial = int(val_inicial)
+
+        val_final = input("Digite o valor final: ")
+        val_final = int(val_final)
+
+        if val_inicial <= 0:
+            print("O valor inicial precisa ser maior que 0")
+            return ValueError
+        elif val_final <= 0:
+            print("O valor final precisa ser maior que 0")
+            return ValueError
+
+        valor_diff = val_final - val_inicial
+        percentual = (abs(valor_diff) / val_inicial) * 100
+
+        if valor_diff < 0:
+            print(
+                f"de {val_inicial} para {val_final}, A variação percentual é -{percentual}%"
+            )
+        else:
+            print(
+                f"de {val_inicial} para {val_final}, A variação percentual é +{percentual}%"
+            )
+
+    except ValueError:
+        print("Os valores inseridos são inválidos")
+
+
 if __name__ == "__main__":
-    calcular_fatorial()
+    os.system("cls" if os.name == "nt" else "clear")
+    # calcula_potencia()
+    varia_percent()
